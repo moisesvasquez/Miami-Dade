b7b51706f74a2db61fdf9b673df7684f909c944e
Moises Vasquez <moisesvasquez@Moisess-MacBook-Pro.local>
2019-03-24 13:38:03 -0400

    Added CSV files

--- MiamiDade.R
+++ MiamiDade.R
@@ -1,9 +1,10 @@
-library(httr)
-library(jsonlite)
-## 
-## Attaching package: 'jsonlite'
-## 
-## The following object is masked from 'package:utils':
-## 
-##     View
-library(lubridate)
+#library(httr)
+#api <- function(path) {
+#  url <- modify_url("https://opendata.arcgis.com/datasets/e1b74e88e203458b8a3b5c93f0ea68a1_0.geojson", path = path)
+#  GET(url)
+#}
+#resp <- api("/repos/hadley/httr")
+#resp
+setwd("~/Documents/R/Miami-Dade")
+Use <- read.csv("Certificates_of_Use_Issued_by_MiamiDade_County__Jan_2003_to_present.csv")
+Occupancy <- read.csv("Certificate_of_Occupancy.csv")
